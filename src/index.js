dccd02d710153827c5b295838400c741cc2453e9
rubenfs <biorubenfs@hotmail.com>
2024-08-24 01:16:41 +0200

    add more examples

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { BrowserBarcodeReader } from '@zxing/library';
 
-const codeReader = new BrowserBarcodeReader();
+const images = [["image1", "result1"], ["image2", "result2"], ["image3", "result3"], ["image4", "result4"]]
 
-console.log('ZXing code reader initialized');
+for (const [image, result] of images) {
+    const codeReader = new BrowserBarcodeReader();
 
-const img = document.getElementById("image");
-const resultEl = document.getElementById("result");
+    console.log('ZXing code reader initialized');
 
-codeReader.decodeFromImage(img)
-    .then(result => {
-        console.log(result);
-        resultEl.textContent = result.text;
-    })
-    .catch(err => {
-        console.error(err);
-        resultEl.textContent = err;
-    });
+    const img = document.getElementById(image);
+    const resultEl = document.getElementById(result);
+
+    codeReader.decodeFromImage(img)
+        .then(result => {
+            console.log(result);
+            resultEl.textContent = result.text;
+        })
+        .catch(err => {
+            console.error(err);
+            resultEl.textContent = err;
+        });
+}
